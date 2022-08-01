77f6b2169ed0f038c71bcf291e4c2644742783cb
andrew-caruso <adcaruso110@gmail.com>
2022-08-01 17:22:38 +0200

    attempted to implement photon scan

--- new_pressure.py
+++ new_pressure.py
@@ -1,4 +1,5 @@
 #import necessary modules 
+from os import POSIX_SPAWN_OPEN
 import random as rnd 
 import time
 import matplotlib.pyplot as plt 
@@ -21,6 +22,7 @@ from numpy import nan as nan
 from numpy import reshape as reshape
 from numpy import abs as abs 
 from numpy import arange as arange 
+from numpy import histogram2d as hist2d 
 from matplotlib.patches import Circle as circle 
 from matplotlib.patches import Wedge as wedge 
 from matplotlib.lines import Line2D as line
@@ -68,12 +70,21 @@ def findPressure(r,m):
 
 #generate photons 
 def photonGeneration(r,m,a_min,a_max,numG): 
+		#for the photon scan 
 	#numG is the number of photons to generate on the ring 
+	#numG may be set to 18 e.g. but use poission distribution to pick a value near or at it
+	numG = rand.poisson(numG) #number of photons for the time resolution from a poission distribution located a numG instead of using rand numbers 
+	tres1 = 280 #picoseconds, time resolution for 1 photon, 280 ps  
+	mu1 = 0 #mean for normal/gaussian distribution   
+	tres = tres1 / sqrt(n) #time resolution for any number of photons 
+	t = rand.normal(mu1,tres) #mean of zero, std of the time resolution (given an numG)
+
+		#for the diaphragm scan, pressure scan, and the pressure selection  
 	chart = zeros(8) #index = sector-1, used as binary 0 or 1 
 	#for a PM of the sector detecting 1 or more photons 
 	#from sector 1 (index 0) to sector 8 (index 7)
 	count = zeros(8) #used as number of photons in each sector 
-	mu = r #mean for normal/gaussian distribution 
+	mu2 = r #mean for normal/gaussian distribution 
 	left = 0
 	right = 0
 	up = 0
@@ -81,7 +92,7 @@ def photonGeneration(r,m,a_min,a_max,numG):
 	#loop through each photon 
 	for i in range(0,numG,1): 
 		phi = rnd.uniform(0,2*pi) #generate random angle
-		r0 = rand.normal(mu,sigma) #generate photon's radius from center
+		r0 = rand.normal(mu2,sigma) #generate photon's radius from center
 		x = r0*cos(phi) + x_off 
 		y = r0*sin(phi) + y_off 
 		r1 = sqrt(x**2 + y**2) 
@@ -136,7 +147,7 @@ def photonGeneration(r,m,a_min,a_max,numG):
 	down = count[3] + count[4] #sector 4 and 5
 	right = count[1] + count[2] #sector 2 and 3
 	left = count[5] + count[6] #sector 6 ad 7 
-	return totCount,up,down,left,right 
+	return totCount,up,down,left,right,t 
 
 #calculate the asymmetries 
 def getAsymmetries(up,down,left,right,totalcount):
@@ -211,7 +222,7 @@ def printing(number):
 #pressure scan 
 def pressureScan(determiner,statvec,mass,numRings,stat5pvec,stat6pvec,stat7pvec,stat8pvec):
 	printing(determiner)
-	for P in bins:  
+	for P in bins1:  
 		#initialization per pressure 
 		stat5p = 0 #for Nc >= 5 stat 5+ 
 		stat6p = 0 #for Nc >= 6 stat 6+
@@ -229,7 +240,7 @@ def pressureScan(determiner,statvec,mass,numRings,stat5pvec,stat6pvec,stat7pvec,
 		#all radii are used because off set of aperture and smearing of particle rings
 		#allows for certain invalid radii to still be inside the aperture to some degree 
 		for i in range(0,numRings):
-			Nc,up,down,left,right = photonGeneration(r,mass,r_min,r_max,18)
+			Nc,up,down,left,right,_ = photonGeneration(r,mass,r_min,r_max,18)
 			stat5p = valuesHist5p(Nc) 
 			stat6p = valuesHist6p(Nc) 
 			stat7p = valuesHist7p(Nc) 
@@ -271,7 +282,7 @@ def pressureSelection(determiner,mass,numRings,hue,name):
 		r = radius(mass,n)
 		print("radius:",r,"cm")
 		for i in range(0,numRings):
-			Nc,up,down,left,right = photonGeneration(r,mass,r_min,r_max,18) 		
+			Nc,up,down,left,right,_ = photonGeneration(r,mass,r_min,r_max,18) 		
 			sumup += up
 			sumdown += down
 			sumleft += left
@@ -297,13 +308,81 @@ def diaphragmScan(w_min,w_max,mass):
 		a_min = r_kaon - w/2.0
 		a_max = r_kaon + w/2.0
 		for x in range(0,1000):
-			num,_,_,_,_ = photonGeneration(r_kaon,mass,a_min,a_max,18)
+			num,_,_,_,_,_ = photonGeneration(r_kaon,mass,a_min,a_max,18)
 			if num >= 6:
 				coincidences[i] += 1 
 			#number of coincidences is total number of photons detected in any of the sectors 
 		i += int(1) 
 	return widths,coincidences
  
+
+#photon scan 
+def photonScan(numG,numR,bool,numG_array):
+	#numG is the number of photons 
+	#numR is the number of rings 
+	#numC is the number of coincidences (total number of photons detected in any sector), rather the number of sectors that made a detection
+	#bool is either 1 to make single plots or 0 to not make single plots
+	#photonScan has two modes bool=1 to make single histogram for only 1 numG and bool=0 to use outputs from multiple undrawn plots to make a 2D histogram
+	radius = r_kaon
+	mass = mka
+	b_min = r_min
+	b_max = r_max
+	num_it = int(numG*numR) #number of iterations
+	print("number of interations:",num_it)
+	coin_array= zeros(num_it)  
+	time_array = zeros(num_it)	
+	fig6,ax6 = plt.subplots(1,2) #create a figure with 1 row 2 columns, i.e. 2 horizontal axes 
+	#mode 1: create single histogram for single numG
+	if bool == 1:
+		#generate 2 histograms for single numG
+		print("Selected photon scan mode 1 (1D histogram)")
+		num = numG 
+		for i in range(0,numR): 
+			numC,_,_,_,_,time= photonGeneration(radius,mass,b_min,b_max,numG)
+			coin_array[i] = numC  
+			time_array[i] = time
+		bins_c,val_c,_ = ax6[0].hist(coin_array,bins_Gscan)
+		x_max1 = max(val_c)
+		y_max1 = max(bins_c)
+		print("sector max:",x_max1,",",y_max1)
+		ax6[0].annotate("numG: %i" % num,(x_max1*0.9,y_max1*0.9),ha='center',va='center')
+		ax6[0].set_xlabel("Number of sectors")
+		ax6[0].set_ylabel("Frequency")
+		#create the time histogram 
+		bins_t,val_t,_ = ax6[1].hist(time_array,bins_Gscan)
+		x_max2 = max(val_t)
+		y_max2 = max(bins_t)
+		print("time max:",x_max2,",",y_max2)
+		ax6[1].annotate("numG: %i" % num,(x_max2*0.8,y_max2*0.9),ha='center',va='center')
+		ax6[1].set_xlabel("time (ps)")
+
+	#mode 2: use outputs from many histograms and many numG to create 2D histogram
+	else: 	
+		#generate 2D histograms for many numG 
+		print("Selected photon scan mode 2 (2D histogram)")
+		coin_array= zeros(numG)  
+		time_array = zeros(numG)	
+		print("coin shape:",shape(coin_array))
+		print("time shape:",shape(time_array))
+		print("numG shape:",shape(numG_array))
+		for num in range(1,numG+1):
+			for i in range(0,numR): 
+				numC,_,_,_,_,time = photonGeneration(radius,mass,b_min,b_max,num)
+				coin_array[num-1] = numC  
+				time_array[num-1] = time
+		#reshape the coin and time arrays 
+		coin_array = reshape(coin_array,(numG,-1))
+		time_array = reshape(time_array,(numG,-1))
+		print("coin shape:",shape(coin_array))
+		print("time shape:",shape(time_array))
+		print("numG shape:",shape(numG_array))
+		'''
+		ax6[1].hist2d(numG_array,coin_array)
+		ax6[0].hist2d(numG_array,time_array) 
+		'''
+
+
+	return coin_array,time_array 
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -322,7 +401,7 @@ sigma = 0.02 #cm smearing of photons per particle ring
 T = 293 #K 
 l = 300 #cm or 3m distance of cone 
 NumPress = int(2E+2) #number of pressures to generate for the pressure scan
-useN2 = 0 #1 means use N2 gas while 0 means use H2 gas  
+useN2 = 1 #1 means use N2 gas while 0 means use H2 gas  
 doScan = 3
 #0 for pressure scan
 #1 for pressure selection (alignment)
@@ -439,7 +518,7 @@ elif useN2 == 0:
 	P_select = 3.7E+6 #baryes or 1.8 bar the selected pressure 
 
 #create bins array for all histograms
-bins = arange(P_min,P_max,step) 
+bins1 = arange(P_min,P_max,step) 
 
 	#print the new parameters 
 print("step size:",step)
@@ -461,16 +540,16 @@ print("selected pressure:",P_select/1E+6,"bar\n")
 
 	#perform the pressure scan
 if doScan == 0:
-	print("Performing Pressure Scan:")
+	print("\tPerforming Pressure Scan:")
 	vec1,vec5p1,vec6p1,vec7p1,vec8p1= pressureScan(1,vec1,mka,num1,vec5p1,vec6p1,vec7p1,vec8p1)
 	vec2,vec5p2,vec6p2,vec7p2,vec8p2= pressureScan(2,vec2,mpr,num2,vec5p2,vec6p2,vec7p2,vec8p2)
 	vec3,vec5p3,vec6p3,vec7p3,vec8p3= pressureScan(3,vec3,mpi,num3,vec5p3,vec6p3,vec7p3,vec8p3)
 	#create the first histogram 
 	#manually make the histograms using numpy.bar instead of numpy.hist
 	fig1, ax1 = plt.subplots(1,1)
-	ax1.bar(bins,vec1,width=step,label=name1,color=color1,align='edge')
-	ax1.bar(bins,vec2,width=step,label=name2,color=color2,align='edge')
-	ax1.bar(bins,vec3,width=step,label=name3,color=color3,align='edge')
+	ax1.bar(bins1,vec1,width=step,label=name1,color=color1,align='edge')
+	ax1.bar(bins1,vec2,width=step,label=name2,color=color2,align='edge')
+	ax1.bar(bins1,vec3,width=step,label=name3,color=color3,align='edge')
 	ax1.set_xlabel("pressure (Ba)")
 	ax1.set_ylabel("frequency")
 	ax1.legend(loc="best") 
@@ -479,18 +558,18 @@ if doScan == 0:
 	#manually make the histograms using numpy.bar instead of numpy.hist
 	#create histograms for Nc 5+,6+,7+,8+ for each particle 
 	fig3,ax3 = plt.subplots(1,1) 
-	ax3.bar(bins,vec5p1,width=step,label="5+",color='magenta')
-	ax3.bar(bins,vec6p1,width=step,label="6+",color='darkred')
-	ax3.bar(bins,vec7p1,width=step,label="7+",color='lime')
-	ax3.bar(bins,vec8p1,width=step,label="8",color='cyan')
-	ax3.bar(bins,vec5p2,width=step,color='magenta')
-	ax3.bar(bins,vec6p2,width=step,color='darkred')
-	ax3.bar(bins,vec7p2,width=step,color='lime')
-	ax3.bar(bins,vec8p2,width=step,color='cyan')
-	ax3.bar(bins,vec5p3,width=step,color='magenta')
-	ax3.bar(bins,vec6p3,width=step,color='darkred')
-	ax3.bar(bins,vec7p3,width=step,color='lime')
-	ax3.bar(bins,vec8p3,width=step,color='cyan')
+	ax3.bar(bins1,vec5p1,width=step,label="5+",color='magenta')
+	ax3.bar(bins1,vec6p1,width=step,label="6+",color='darkred')
+	ax3.bar(bins1,vec7p1,width=step,label="7+",color='lime')
+	ax3.bar(bins1,vec8p1,width=step,label="8",color='cyan')
+	ax3.bar(bins1,vec5p2,width=step,color='magenta')
+	ax3.bar(bins1,vec6p2,width=step,color='darkred')
+	ax3.bar(bins1,vec7p2,width=step,color='lime')
+	ax3.bar(bins1,vec8p2,width=step,color='cyan')
+	ax3.bar(bins1,vec5p3,width=step,color='magenta')
+	ax3.bar(bins1,vec6p3,width=step,color='darkred')
+	ax3.bar(bins1,vec7p3,width=step,color='lime')
+	ax3.bar(bins1,vec8p3,width=step,color='cyan')
 	ax3.set_xlabel("pressure (Ba)")
 	ax3.set_ylabel("frequency")
 	ax3.set_title("x_off: %fcm y_off: %fcm std: %fcm" % (x_off,y_off,sigma))
@@ -498,7 +577,7 @@ if doScan == 0:
 
 	#perform the selected pressure alignment (rings graph)
 elif doScan == 1:
-	print("Performing Pressure Selection:")
+	print("\tPerforming Pressure Selection:")
 	kaonring,up1,down1,left1,right1 = pressureSelection(1,mka,num1,color1,name1)
 	protonring,up2,down2,left2,right2= pressureSelection(2,mpr,num2,color2,name2)
 	pionring,up3,down3,left3,right3= pressureSelection(3,mpi,num3,color3,name3)
@@ -585,7 +664,7 @@ elif doScan == 1:
 	#as width increased from 0.4 mm to 2 mm 
 	#P = P_max of the pion peak 
 if doScan == 2:
-	print("Performing Diaphragm Scan:")
+	print("\tPerforming Diaphragm Scan:")
 	'''
 	#perform pressure scan of pion 
 	_,_,vec6p3,_,_= pressureScan(3,vec3,mpi,num3,vec5p3,vec6p3,vec7p3,vec8p3)
@@ -626,10 +705,20 @@ if doScan == 2:
 
 	#perform photon scan
 if doScan == 3:
-	print("Performing Photon Scan")
+	numP = int(18) #number of photons
+	numRings = 1000 #number of rings to generate per photon 
+	bins_Gscan = 100 
+	gen_single = 0 #to generate histogram for single numG else, generate histogram for many numG  
+	print("\tPerforming Photon Scan")
+	print("Number of photons:",numP)
+	print("Number of rings per photon:",numRings)
+	print("bins:",bins_Gscan)
+	numPVec = arange(1,numP+1) #from 1 to numP
+	photonScan(numP,numRings,gen_single,numPVec)
+
 
 	#stop timer
 print("\n\tRun time:")
 print("%s seconds" % (time.time()-start_time))
 print("%s minutes" % ((time.time()-start_time)/60))
-plt.show()
+#plt.show()
